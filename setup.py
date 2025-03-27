1e1eadaae4ce06ec2d2b237f03afe4cf9d3e0c5a
RBX <rbx.qb@proton.me>
2025-03-27 09:23:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="BQL",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
         "qiskit",
         "matplotlib",
-        "pytest"
+        "numpy"
     ],
+    author="Rafael Brayan",
+    description="Bosonic Quantum Language (BQL) para emaranhamento quântico",
+    url="https://github.com/rbx.qb/BQL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
 
