6147df736a12bd06c0e473cd647f70eb48688889
RBX <rbx.qb@proton.me>
2025-03-27 09:19:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BQL",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "qiskit",
+        "matplotlib",
+        "pytest"
+    ],
+)
 
