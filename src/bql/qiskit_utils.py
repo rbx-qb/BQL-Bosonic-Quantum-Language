966b21a210b321b0d2666d5c4065ed7bd17d8742
RBX <rbx.qb@proton.me>
2025-03-27 09:16:25 -0300

    Update qiskit_utils.py

--- src/bql/qiskit_utils.py
+++ src/bql/qiskit_utils.py
@@ -1,1 +1,20 @@
+from qiskit import QuantumCircuit, Aer, execute
+import matplotlib.pyplot as plt
+
+def create_entangled_pair():
+    qc = QuantumCircuit(2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure_all()
+
+    simulator = Aer.get_backend('aer_simulator')
+    result = execute(qc, simulator).result()
+    return result.get_counts()
+
+def plot_results(counts):
+    plt.bar(counts.keys(), counts.values())
+    plt.xlabel("State")
+    plt.ylabel("Count")
+    plt.title("Quantum Entanglement Measurement")
+    plt.show()
 
