617f634094c65c3ea6eca2af6d8a78b528bc9e71
RBX <rbx.qb@proton.me>
2025-03-27 09:18:25 -0300

    Update test_bql.py

--- tests/test_bql.py
+++ tests/test_bql.py
@@ -1,1 +1,14 @@
+from src.bql.interpreter import BQLInterpreter
+
+def test_create_register():
+    bql = BQLInterpreter()
+    bql.execute("CREATE q1 4")
+    assert "q1" in bql.registers
+    assert bql.registers["q1"]["levels"] == 4
+
+def test_entangle():
+    bql = BQLInterpreter()
+    bql.execute("CREATE q1 4")
+    bql.execute("CREATE q2 4")
+    bql.execute("ENTANGLE q1 q2")
 
